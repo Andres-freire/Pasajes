5fed2e57c6e6eb85b6f6e595945b9dcb3ab60055
Andres-freire <lafreire4@espe.edu.ec>
2024-02-16 14:42:31 -0500

    first

---
+++ src/main/java/com/mycompany/pasajes/Pasajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pasajes;
+
+/**
+ *
+ * @author labctr
+ */
+public class Pasajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
