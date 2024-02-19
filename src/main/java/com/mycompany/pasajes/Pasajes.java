5bb7b384f57c92aad071a81bde3a977223c21f80
Andres-freire <lafreire4@espe.edu.ec>
2024-02-18 19:38:13 -0500

    Frontend terminado

--- src/main/java/com/mycompany/pasajes/Pasajes.java
+++ src/main/java/com/mycompany/pasajes/Pasajes.java
@@ -1,29 +1,13 @@
 package com.mycompany.pasajes;
-
-import backend.Bus;
-import backend.Precio;
-import backend.Taxi;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatGitHubIJTheme;
 import frontend.MenuJFrame;
 
 public class Pasajes {
 
     public static void main(String[] args) {
+        FlatGitHubIJTheme.setup();
         MenuJFrame menu = new MenuJFrame();
         menu.setLocationRelativeTo(null);
         menu.setVisible(true);
-       
-        Precio precio = new Precio();
-       
-        Taxi taxi = new Taxi();
-      
-        taxi.setDistancia(20.0); 
-       
-        Bus bus = new Bus();
-     
-        bus.setDistancia(20.0);   
-        double precioTaxi = precio.calcular(taxi);
-        System.out.println("Precio del taxi: " + precioTaxi);
-        double precioBus = precio.calcular(bus);
-        System.out.println("Precio del bus: " + precioBus);
     }
 }
